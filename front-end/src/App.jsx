4e359c6869f33aa5468f99b1be8017ef242ce2d4
Kally Dossa <85954491+Kally-Dossa@users.noreply.github.com>
2025-09-23 16:57:00 +0300

    update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
+import MainPage from "./components/MainPage";
 import SearchWeather from "./components/SearchWeather";
 import ChartWeather from "./components/ChartWeather";
 import Sidebar from "./components/Sidebar";
 import "./App.css";
 
 function App() {
-  const [activeTab, setActiveTab] = useState("search-weather");
+  const [activeTab, setActiveTab] = useState("main-page");
 
   return (
-    <div className="app-container">
-      <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
-      <div className="main-content">
-        {activeTab === "search-weather" && <SearchWeather />}
-        {activeTab === "chart-weather" && <ChartWeather />}
+    <>
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="background-video"
+        src="/sky.mp4"
+        type="video/mp4"
+      />
+      <div className="app-container">
+        <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
+        <div className="main-content">
+          {activeTab === "main-page" && <MainPage />}
+          {activeTab === "search-weather" && <SearchWeather />}
+          {activeTab === "chart-weather" && <ChartWeather />}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
